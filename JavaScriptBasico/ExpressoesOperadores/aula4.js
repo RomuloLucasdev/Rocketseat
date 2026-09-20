872c0b908f43ca880f134bc206dba6867081ab7a
RomulooDev23 <23romulolucas@gmail.com>
2026-09-20 07:21:24 +0100

    maior que , menor que ,  maior ou igual a e menor ou igual .

---
+++ JavaScriptBasico/ExpressoesOperadores/aula4.js
@@ -0,0 +1,22 @@
+let balance = 500;
+let payment = 120;
+
+// > Maior que (>) - compara se o valor da esquerda é maior que o da direita
+console.log("## Maior que ##")
+console.log(balance > payment) // true
+console.log(payment > balance) // false
+
+// < Menor que (<) - compara se o valor da esquerda é menor que o da direita
+console.log("## Menor que ##")
+console.log(balance < payment) // false
+console.log(payment < balance) // true
+
+// >= Maior ou igual (>=) - compara se o valor da esquerda é maior ou igual ao da direita
+console.log("## Maior ou igual ##")
+console.log(balance >= payment) // true
+console.log(payment >= balance) // false
+
+// <= Menor ou igual (<=) - compara se o valor da esquerda é menor ou igual ao da direita
+console.log("## Menor ou igual ##")
+console.log(balance <= payment) // false
+console.log(payment <= balance) // true
