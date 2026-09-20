58460d6c6a700cc9525d580ca011168e6761451b
RomulooDev23 <23romulolucas@gmail.com>
2026-09-20 07:04:27 +0100

    Operadores Aritmétricos

---
+++ JavaScriptBasico/ExpressoesOperadores/aula1.js
@@ -0,0 +1,7 @@
+// Expressão = é uma combinação  de valores, variáveis, operadores e chamadas de funções que são avaliadas e produzem um valor.
+
+// Operadores = são símbolos que permitem realizar operações entre valores.
+
+let sum = 5 + 5 // 10
+
+console.log(sum);
---
+++ JavaScriptBasico/ExpressoesOperadores/aula2.js
@@ -0,0 +1,34 @@
+// Operadores aritméticos
+
+
+let num1 = 10;
+let num2 = 5;
+
+//Soma
+console.log(num1 + num2); // 15
+
+//Subtração
+console.log(num1 - num2); // 5
+
+//Multiplicação
+console.log(num1 * num2); // 50
+
+//Divisão
+console.log(num1 / num2); // 2
+
+//Resto da divisão
+console.log(num1 % num2); // 0 
+
+//Potenciação
+console.log(num1 ** num2); // 100000 
+
+// Incremento
+num1++;
+console.log(num1); // 11 
+
+// Decremento
+num2--;
+console.log(num2); // 4
+
+// Concatenação
+console.log("num1" + "num2"); // num1num2
