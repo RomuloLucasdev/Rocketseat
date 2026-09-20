d2b55e360ffb1de8247c732dcd03159f88dc1a1e
RomulooDev23 <23romulolucas@gmail.com>
2026-09-20 07:27:55 +0100

    Operadore de atribuição

---
+++ JavaScriptBasico/ExpressoesOperadores/aula5.js
@@ -0,0 +1,23 @@
+
+let value = 1;
+
+//Incremeneto (+=) - incrementa o valor da variável
+value +=1;
+console.log(value); // 2
+
+//Decremento (-=) - decrementa o valor da variável
+value -=1;
+console.log(value); // 1
+
+//Multiplicação (*=) - multiplica o valor da variável
+value *=2;
+console.log(value); // 2 
+
+//Divisão (/=) - divide o valor da variável
+value /=2;
+console.log(value); // 1
+
+//Resto da divisão (%=) - calcula o resto da divisão do valor da variável
+value %=2;
+console.log(value); // 1
+
